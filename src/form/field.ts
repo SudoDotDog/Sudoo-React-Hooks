27f35980d0b694647fb3148b1cd3d17d34aac417
WMXPY <wm@mengw.io>
2021-06-20 14:43:26 -0500

    release 14.0 with form field hook

---
+++ src/form/field.ts
@@ -0,0 +1,63 @@
+/**
+ * @author WMXPY
+ * @namespace ReactHooks_Form
+ * @description Field
+ */
+
+import * as React from "react";
+
+export type FormFieldStates<T, H> = {
+
+    readonly data: T;
+    readonly hint?: H;
+
+    readonly erroring: boolean;
+
+    readonly update: (newData: T, clearError?: boolean) => void;
+
+    readonly errorHint: (hint: H) => void;
+    readonly clearError: () => void;
+};
+
+export const useFormField = <T extends any = any, H extends any = string>(
+    initialData: T,
+    initialHint?: H,
+    initialError?: boolean,
+): FormFieldStates<T, H> => {
+
+    const [fieldData, setFieldData] = React.useState<T>(initialData);
+    const [fieldHint, setFieldHint] = React.useState<H | undefined>(initialHint);
+    const [erroring, setErroring] = React.useState(initialError ? true : false);
+
+    const clearError = () => {
+
+        setFieldHint(undefined);
+        setErroring(false);
+    };
+
+    const update = (newData: T, shouldClearError?: boolean) => {
+
+        setFieldData(newData);
+        if (shouldClearError) {
+            clearError();
+        }
+    };
+
+    const errorHint = (hint: H) => {
+
+        setFieldHint(hint);
+        setErroring(true);
+    };
+
+    return {
+
+        data: fieldData,
+        hint: fieldHint,
+
+        erroring,
+
+        update,
+        errorHint,
+        clearError,
+    };
+};
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export * from "./data/async/hook";
 export * from "./data/async/states";
 export * from "./data/cache/cache";
 export * from "./data/declare";
+export * from "./form/field";
 export * from "./states/anchor";
 export * from "./states/loading";
 export * from "./states/open";
