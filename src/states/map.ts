c7d6c8b196a5c88098397f4d008e84c2bde0734a
WMXPY <wm@mengw.io>
2021-07-20 21:37:55 -0500

    release with new hooks

--- src/states/map.ts
+++ src/states/map.ts
@@ -9,11 +9,14 @@ import * as React from "react";
 export type MapStates<Key extends string | number | symbol, Element> = {
 
     readonly map: Record<Key, Element>;
-    readonly length: number;
+
+    readonly getLength: () => number;
+    readonly getEntries: () => Array<[Key, Element]>;
 
     readonly get: (key: Key) => Element | undefined;
     readonly set: (key: Key, value: Element) => void;
 
+    readonly merge: (newMap: Partial<Record<Key, Element>>) => void;
     readonly replace: (newMap: Record<Key, Element>) => void;
 };
 
@@ -26,7 +29,9 @@ export const useMap = <Key extends string | number | symbol = string, Element ex
     return {
 
         map,
-        length: Object.keys(map).length,
+
+        getLength: () => Object.keys(map).length,
+        getEntries: () => [...(Object.entries(map) as any)],
 
         get: (key: Key) => map[key],
         set: (key: Key, value: Element) => setMap({
@@ -34,6 +39,10 @@ export const useMap = <Key extends string | number | symbol = string, Element ex
             [key]: value,
         }),
 
+        merge: (newMap: Partial<Record<Key, Element>>) => setMap({
+            ...map,
+            ...newMap,
+        }),
         replace: (newMap: Record<Key, Element>) => setMap(newMap),
     };
 };
