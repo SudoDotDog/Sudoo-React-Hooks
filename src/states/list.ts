3d20119f59bea770b6e4e25b9b1ba0c7c3ac62d4
WMXPY <wm@mengw.io>
2021-08-20 16:38:10 -0500

    improve list

--- src/states/list.ts
+++ src/states/list.ts
@@ -14,6 +14,11 @@ export type ListStates<Element> = {
     readonly get: (index: number) => Element | undefined;
     readonly set: (index: number, value: Element) => void;
 
+    readonly push: (value: Element) => void;
+    readonly pop: () => Element | undefined;
+    readonly unshift: (value: Element) => void;
+    readonly shift: () => Element | undefined;
+
     readonly replace: (list: Element[]) => void;
 };
 
@@ -32,6 +37,25 @@ export const useList = <Element extends any = any>(initialElements: Element[] = 
                 return index === itemIndex ? value : item;
             }),
         ),
+
+        push: (value: Element) => setList([...list, value]),
+        pop: () => {
+
+            const newList: Element[] = [...list];
+            const result: Element | undefined = newList.pop();
+
+            setList(newList);
+            return result;
+        },
+        unshift: (value: Element) => setList([value, ...list]),
+        shift: () => {
+
+            const newList: Element[] = [...list];
+            const result: Element | undefined = newList.shift();
+
+            setList(newList);
+            return result;
+        },
 
         replace: (newList: Element[]) => setList(newList),
     };
