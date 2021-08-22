c123bd139a9a5be49a8c8db0c61d963249590d5f
WMXPY <wm@mengw.io>
2021-08-22 16:07:26 -0500

    use old pattern

--- src/states/vector.ts
+++ src/states/vector.ts
@@ -26,13 +26,11 @@ export type VectorStates = {
 
 export const useVector = (init?: VectorInitial): VectorStates => {
 
-    const fixedInit: VectorInitial = typeof init === 'undefined'
-        ? {}
-        : init;
+    const fixedInit: VectorInitial = typeof init === 'undefined' ? {} : init;
 
-    const [X, setX] = React.useState(fixedInit.x ?? 0);
-    const [Y, setY] = React.useState(fixedInit.y ?? 0);
-    const [Z, setZ] = React.useState(fixedInit.z ?? 0);
+    const [X, setX] = React.useState(typeof fixedInit.x === 'number' ? fixedInit.x : 0);
+    const [Y, setY] = React.useState(typeof fixedInit.y === 'number' ? fixedInit.y : 0);
+    const [Z, setZ] = React.useState(typeof fixedInit.z === 'number' ? fixedInit.z : 0);
 
     return {
 
