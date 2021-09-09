fd37bc2db89f6135449c91ab34a2fdaccf06ff8d
WMXPY <wm@mengw.io>
2021-09-09 15:06:33 -0500

    improve hisotry stradgy

--- src/states/history.ts
+++ src/states/history.ts
@@ -13,6 +13,9 @@ export type HistoryStates<T> = {
 
     readonly updateLatest: () => void;
 
+    readonly push: (value: T) => void;
+    readonly pop: () => void;
+
     readonly pushAndUpdate: (value: T) => void;
     readonly popAndUpdate: () => void;
 };
@@ -21,35 +24,50 @@ export const useHistory = <T>(setFunction: (value: T) => void, initialValue?: T)
 
     const [histories, setHistories] = React.useState<T[]>(initialValue ? [initialValue] : []);
 
+    const updateLatest = () => {
+        setFunction(histories[histories.length - 1]);
+    };
+
+    const push = (value: T): void => {
+        setHistories((previousHistories: T[]) => {
+            return [...previousHistories, value];
+        });
+    };
+
+    const pop = (): void => {
+        if (histories.length >= 1) {
+            setHistories((previousHistories: T[]) => {
+                return previousHistories.slice(0, previousHistories.length - 1);
+            });
+        }
+    };
+
+    const pushAndUpdate = (value: T): void => {
+        setFunction(value);
+        push(value);
+    };
+
+    const popAndUpdate = (): void => {
+        if (histories.length > 1) {
+            setFunction(histories[histories.length - 2]);
+        } else if (histories.length === 1) {
+            setFunction(histories[0]);
+        }
+
+        pop();
+    };
+
     return {
 
         size: histories.length,
         histories,
 
-        updateLatest: () => {
+        updateLatest,
 
-            setFunction(histories[histories.length - 1]);
-        },
-        pushAndUpdate: (value: T): void => {
+        push,
+        pop,
 
-            setFunction(value);
-            setHistories((previousHistories: T[]) => {
-                return [...previousHistories, value];
-            });
-        },
-        popAndUpdate: (): void => {
-
-            if (histories.length > 1) {
-                setFunction(histories[histories.length - 2]);
-            } else if (histories.length === 1) {
-                setFunction(histories[0]);
-            }
-
-            if (histories.length >= 1) {
-                setHistories((previousHistories: T[]) => {
-                    return previousHistories.slice(0, previousHistories.length - 1);
-                });
-            }
-        },
+        pushAndUpdate,
+        popAndUpdate,
     };
 };
